d7381b4c786c713699d6287e72e01143a31d4fb7
Dan <daniel.guinita@cit.edu>
2023-12-13 16:26:25 +0800

    2nd commit, implementation needs fixing

---
+++ .venv/app.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, request, redirect, url_for, flash
+
+
+app = Flask(__name__, template_folder='todolist')
+
+todolist = [{"todo": "task", "done": False}]
+
+@app.route('/')
+def index():
+    return render_template("index.html", todolist=todolist)
+
+@app.route("/add", methods=["POST"])
+def add():
+    todo = request.form['todo']
+    todolist.append({"task": todo, "done": False})
+    return redirect(url_for('index'))
+
+@app.route("/edit", methods=["GET", "POST"])
+def edit(index):
+    todo = todolist[index]
+    if request.method == "POST":
+        todo["todo"] = request.form['todo']
+        return redirect(url_for('index'))
+    else:
+        return render_template("edit.html", todo=todo, index=index)
+
+@app.route("/delete/<int:index>")
+def delete(index):
+    del todo
+    return redirect(url_for('index'))
+
+@app.route("/check/<int:index>")
+def check(index):
+    todolist[index]["done"] = not todolist[index]["done"]
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
