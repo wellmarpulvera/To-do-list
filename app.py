806210aa12a969e886e7631ac477a829e55643a2
Dan <daniel.guinita@cit.edu>
2023-12-15 22:37:54 +0800

    145615616

--- app.py
+++ app.py
@@ -1,7 +1,28 @@
 import json
+from flask_mysqldb import MySQL
+from database import set_mysql
 from flask import Flask, render_template, request, redirect, url_for
+from dotenv import load_dotenv
 
-app = Flask(__name__, template_folder='todolist')
+load_dotenv()
+import os
+
+# create the flask app
+app = Flask(__name__, template_folder=os.path.abspath('todolist'))
+
+
+# Required
+app.config["MYSQL_HOST"] = os.getenv("MYSQL_HOST")
+app.config["MYSQL_PORT"] = os.getenv("MYSQL_PORT")
+app.config["MYSQL_USER"] = os.getenv("MYSQL_USER")
+app.config["MYSQL_PASSWORD"] = os.getenv("MYSQL_PASSWORD")
+app.config["MYSQL_DB"] = os.getenv("MYSQL_DB")
+# Extra configs, optional but mandatory for this project:
+app.config["MYSQL_CURSORCLASS"] = os.getenv("MYSQL_CURSORCLASS")
+app.config["MYSQL_AUTOCOMMIT"] = True if os.getenv("MYSQL_AUTOCOMMIT") == "true" else False
+
+mysql = MySQL(app)
+set_mysql(mysql)
 
 # Load tasks from a JSON file if it exists, otherwise initialize with a default task
 try:
