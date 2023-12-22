73e31c05b7c25aa2f9980a512bca8c7430b81e62
Dan <daniel.guinita@cit.edu>
2023-12-22 23:34:49 +0800

    schema update and index add

--- schema.sql
+++ schema.sql
@@ -21,6 +21,25 @@ SELECT t.id, t.title, t.done, c.`name` AS category_name
 FROM tasks t
 JOIN categories c ON t.category_id = c.id;
 
+CREATE PROCEDURE add_task(
+  IN title_val VARCHAR(255),
+  IN category_id_val INT
+)
+BEGIN
+  INSERT INTO tasks (title, category_id)
+  VALUES (title_val, category_id_val);
+END //
+
+
+CREATE PROCEDURE edit_task(
+    IN task_id INT,
+    IN new_title VARCHAR(255)
+)
+BEGIN
+    UPDATE tasks
+    SET title = new_title
+    WHERE id = task_id;
+END //
 
 
 
