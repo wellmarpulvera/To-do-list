9b1315625aedf907fe5c7dd7a7c31a4531823515
roxsand <132316834+roxsand@users.noreply.github.com>
2023-12-22 22:58:53 +0800

    App.py first commit

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask_mysqldb import MySQL
+from flask import Flask, render_template, request, redirect, url_for
+from dotenv import load_dotenv
+
+load_dotenv()
+import os
+
+# create the flask app
+app = Flask(__name__, template_folder='todolist')
+
+# Required
+app.config["MYSQL_HOST"] = os.getenv("MYSQL_HOST")
+app.config["MYSQL_PORT"] = int(os.getenv("MYSQL_PORT"))
+app.config["MYSQL_USER"] = os.getenv("MYSQL_USER")
+app.config["MYSQL_PASSWORD"] = os.getenv("MYSQL_PASSWORD")
+app.config["MYSQL_DB"] = os.getenv("MYSQL_DB")
+app.config["MYSQL_AUTOCOMMIT"] = True
+app.config["MYSQL_CURSORCLASS"] = "DictCursor"
+
+mysql = MySQL(app)
