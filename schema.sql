cf39e1dd25023ef369b2bc454cd2e08aa7f3002e
roxsand <132316834+roxsand@users.noreply.github.com>
2023-12-22 21:25:56 +0800

    Schema sql, Create Table and VIEWS

--- schema.sql
+++ schema.sql
@@ -1,17 +1,26 @@
--- Create Tasks Table
-CREATE TABLE IF NOT EXISTS tasks (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(255) NOT NULL,
-    done BOOLEAN DEFAULT 0,
-    category_id INT,
-    FOREIGN KEY (category_id) REFERENCES categories(id)
-);
+CREATE SCHEMA 'todolist_im' ;
 
--- Create Catefories Table
-CREATE TABLE IF NOT EXISTS categories (
+-- Create Categories Table
+CREATE TABLE categories (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL
 );
+
+-- Create Tasks Table
+CREATE TABLE tasks (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    done BOOLEAN DEFAULT 0,
+    category_id INT NOT NULL,
+    FOREIGN KEY (category_id) REFERENCES categories(id) ON DELETE CASCADE
+);
+
+-- VIEWS
+CREATE VIEW task_view AS
+SELECT t.id, t.title, t.done, c.`name` AS category_name
+FROM tasks t
+JOIN categories c ON t.category_id = c.id;
+
 
 
 
